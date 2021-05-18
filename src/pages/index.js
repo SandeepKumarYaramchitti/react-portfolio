705b93e9bfe2e84bd9d404480359677bcb1a8438
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-05-18 15:40:12 -0400

    more updates

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {SideBar}from '../components/sidebar'
+import NavBar from '../components/Navbar/index'
+
+export const Home = () => {
+    return (
+        <>
+                <SideBar />
+                <NavBar />
+        </>
+
+    )
+}
